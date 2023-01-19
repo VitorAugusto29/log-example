d24635323c63cfb2f71e0ea2e33644a50928f44b
VitorAugusto29 <vitoraugusto29@hotmail.com>
2023-01-19 00:06:15 -0300

    ex3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,22 +55,12 @@ class HomeController extends Controller
          logger()->ERRO('Divisor zero!');
         }else{
             $div = $num1 / $num2;
-        logger()->INFORMAÇÃO('Div feita');
+         logger()->info('Div feita');
             return $div;
         }
 
     }
-    public function mult($num1, $num2){
    
-               
-        if ($num1 = 0 $num2 = 0) {
-
-             logger()->WARNING('Negativo');
-         }
-         
-
-         
-    }
 
     
         
